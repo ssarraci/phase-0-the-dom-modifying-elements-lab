b064bf1fbaddd65e8036b851f5fdc450175945b4
Storm <storm.sarracino01@gmail.com>
2021-05-23 14:30:11 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const main = document.querySelector('#main')
+
+main.remove()
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+
+newHeader.innerHTML = "Storm is the champion"
+
+document.body.append(newHeader)
